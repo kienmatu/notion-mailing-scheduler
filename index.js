1ab9fcfc3f79f7c6e22b44c02a70cc9f071e0759
Kien Dinh <kienmatu1@gmail.com>
2023-09-09 10:48:37 +0700

    Init the project: sending email

---
+++ index.js
@@ -0,0 +1,35 @@
+const nodemailer = require("nodemailer");
+
+const userEmail = process.env.SENDING_EMAIL;
+const password = process.env.SENDING_PASSWORD;
+const destinationEmail = process.env.DESTINATION_EMAIL;
+
+if (!userEmail || !password || !destinationEmail) {
+  console.error(
+    "You must provide sending email, password and the destination email"
+  );
+  process.exit(1);
+}
+
+let mailTransporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: userEmail,
+    pass: password,
+  },
+});
+
+let mailDetails = {
+  from: userEmail,
+  to: destinationEmail,
+  subject: "Scheduled vocabulary review",
+  text: "Node.js testing mail for GeeksforGeeks",
+};
+
+mailTransporter.sendMail(mailDetails, function (err, data) {
+  if (err) {
+    console.error("Can't send email: ", err.message);
+  } else {
+    console.log("Email sent successfully");
+  }
+});
